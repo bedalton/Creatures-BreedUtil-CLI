8bef421f2906f59658e4587e9a47cd66b56d4cb5
daniel <dsbadal@gmail.com>
2022-12-05 07:25:37 -0800

    Use build in coroutine Dispatchers

---
+++ src/jvmMain/kotlin/bedalton/creatures/breed/converter/cli/internal/coroutines.jvm.kt
@@ -0,0 +1,8 @@
+package bedalton.creatures.breed.converter.cli.internal
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+internal actual val MainDispatcher: CoroutineDispatcher get() = Dispatchers.Main
+
+internal actual val BackgroundDispatcher get() = Dispatchers.IO
--- src/macosX64Main/kotlin/bedalton/creatures/breed/converter/cli/internal/coroutines.macos.kt
+++ src/macosX64Main/kotlin/bedalton/creatures/breed/converter/cli/internal/coroutines.macos.kt
@@ -3,6 +3,6 @@ package bedalton.creatures.breed.converter.cli.internal
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
-internal actual val MainDispatcher get() = Dispatchers.Default
+internal actual val MainDispatcher: CoroutineDispatcher get() = Dispatchers.Main
 
 internal actual val BackgroundDispatcher get() = Dispatchers.Default
